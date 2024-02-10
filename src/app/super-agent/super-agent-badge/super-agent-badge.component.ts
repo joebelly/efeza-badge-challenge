6abb1b3b974812f7a6e8bc4f57ef5a22bbb7ba28
dondelice257 <dondelicedushime@gmail.com>
2024-02-11 01:24:16 +0200

    enable saving badge as a picture and as a pdf

--- src/app/super-agent/super-agent-badge/super-agent-badge.component.ts
+++ src/app/super-agent/super-agent-badge/super-agent-badge.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-super-agent-badge',
@@ -11,7 +13,10 @@ export class SuperAgentBadgeComponent implements OnInit {
   badgeForm!: FormGroup | any;
   imageUrl: any;
   isModalShown:boolean=false
+
+  isMenuShown:boolean=false
   @ViewChild('myModal') myModal: ElementRef | any;
+  
 
   
   constructor() {
@@ -48,6 +53,85 @@ export class SuperAgentBadgeComponent implements OnInit {
 
 
   }
+
+  showMenu(){
+    this.isMenuShown=!this.isMenuShown
+  }
+
+
+  saveAsImage() {
+    const modalContent = this.myModal.nativeElement;
+    html2canvas(modalContent).then(canvas => {
+      
+      const image = canvas.toDataURL('image/png');
+
+      
+      const link = document.createElement('a');
+      link.href = image;
+      link.download = 'badge.png'; 
+      link.click(); 
+    });
+  }
+
+  saveAsPDF(pageSize: string) {
+    const modalContent = this.myModal.nativeElement;
+  
+    // Determine page dimensions based on user selection
+    let pageWidth, pageHeight;
+    switch (pageSize) {
+      case 'A4':
+        pageWidth = 297; // Swap width and height for landscape orientation
+        pageHeight = 210;
+        break;
+      case 'A5':
+        pageWidth = 210; // Swap width and height for landscape orientation
+        pageHeight = 148;
+        break;
+      case 'A6':
+        pageWidth = 148; // Swap width and height for landscape orientation
+        pageHeight = 105;
+        break;
+      default:
+        pageWidth = 297; // Default to A4 landscape
+        pageHeight = 210;
+        break;
+    }
+  
+    // Create new jsPDF instance with landscape orientation and specified page dimensions
+    const pdf = new jsPDF({
+      orientation: 'landscape',
+      unit: 'mm',
+      format: [pageWidth, pageHeight]
+    });
+  
+    // Add HTML content (badge section) to the PDF document
+    const options = {
+      callback: (pdf:any) => {
+        // Save PDF document
+        pdf.save('badge.pdf');
+      }
+    };
+  
+    pdf.html(modalContent, options);
+  }
+
+
+  printModalContent() {
+    const modalContent = this.myModal.nativeElement.innerHTML;
+    const printWindow :any= window.open('', '_blank');
+    printWindow.document.write('<html><head><title>Print</title></head><body>');
+    printWindow.document.write(modalContent);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();
+  }
+
+  
+  
+  
+  
+  
+  
 
   closeModal(event: MouseEvent): void {
     if (!(event.target as HTMLElement).closest('.inner-container')) {
