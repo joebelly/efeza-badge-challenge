b1d8e737ae48ddcf3faa4eb597726836b1e6cecb
dondelice257 <dondelicedushime@gmail.com>
2024-02-10 22:25:08 +0200

    add badge popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SuperAgentBadgeComponent } from './super-agent/super-agent-badge/super-agent-badge.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SuperAgentBadgeComponent } from './super-agent/super-agent-badge/super-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/super-agent/super-agent-badge/super-agent-badge.component.ts
+++ src/app/super-agent/super-agent-badge/super-agent-badge.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-super-agent-badge',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuperAgentBadgeComponent implements OnInit {
 
-  constructor() { }
+  badgeForm!: FormGroup | any;
+  imageUrl: any;
+  isModalShown:boolean=false
+  @ViewChild('myModal') myModal: ElementRef | any;
+
+  
+  constructor() {
+    this.badgeForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      superAgentCode: new FormControl('', Validators.required),
+      phoneNumber: new FormControl('', Validators.required),
+      photo: new FormControl('', Validators.required)
+    });
+  }
 
   ngOnInit() {
   }
 
+  // Function to handle image preview
+  previewImage(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.imageUrl = e.target.result;
+
+        this.badgeForm.patchValue({
+          photo:this.imageUrl
+        })
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  checkGlobalForm(){
+
+
+  }
+
+  closeModal(event: MouseEvent): void {
+    if (!(event.target as HTMLElement).closest('.inner-container')) {
+        this.isModalShown = false;
+    }
+}
 }
